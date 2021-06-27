de627d460c5332b1eed81ac29d7ecfce12040dfc
fisunoff <fisunoff@yandex.ru>
2021-06-27 10:02:10 +0300

    fixed language bug

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 import time
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_buy_btn(browser):
